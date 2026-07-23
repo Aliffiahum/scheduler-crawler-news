bfdfd7ee10bc9295d15ef2e67c9f7e2d7a113c0e
Aliffiahum <146148815+Aliffiahum@users.noreply.github.com>
2026-07-23 22:16:30 +0700

    Update connection.py

--- app/database/connection.py
+++ app/database/connection.py
@@ -4,47 +4,15 @@ from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-# =====================================================
-# Load Environment Variables
-# =====================================================
-
 load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-if DATABASE_URL is None:
-    raise ValueError("DATABASE_URL tidak ditemukan pada file .env")
-
-# =====================================================
-# Database Engine
-# =====================================================
+print("DATABASE_URL repr =", repr(DATABASE_URL))
+print("Length =", len(DATABASE_URL))
 
 engine = create_engine(
     DATABASE_URL,
     pool_pre_ping=True,
     future=True
 )
-
-# =====================================================
-# Session Factory
-# =====================================================
-
-SessionLocal = sessionmaker(
-    bind=engine,
-    autoflush=False,
-    autocommit=False,
-    expire_on_commit=False,
-)
-
-# =====================================================
-# Get Database Session
-# =====================================================
-
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
-print(DATABASE_URL)
